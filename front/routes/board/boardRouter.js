1ee17eb1ded43d1ef37510842615492633da106d
ash991213 <ads1213@nate.com>
2022-03-23 10:34:01 +0900

    feat:board_write(#1)

--- front/routes/board/boardRouter.js
+++ front/routes/board/boardRouter.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const boardController = require('./boardController.js')
 
-
+router.get('/write',boardController.write);
 
 module.exports = router;
