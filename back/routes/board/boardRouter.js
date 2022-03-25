75dffa6eea18967995f8efbb7195ba4c34019a34
ash991213 <ads1213@nate.com>
2022-03-25 11:00:28 +0900

    feat:board_list(#20)

--- back/routes/board/boardRouter.js
+++ back/routes/board/boardRouter.js
@@ -4,6 +4,8 @@ const boardController = require('./boardController.js')
 
 router.post('/write',boardController.write);
 router.post('/view',boardController.view);
+router.post('/mainList',boardController.mainList);
+router.post('/subList',boardController.subList);
 router.get('/edit',boardController.GetEdit);
 router.post('/edit',boardController.PostEdit);
 router.post('/delete',boardController.delete);
