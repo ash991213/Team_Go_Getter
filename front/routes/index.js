06139691320f4ac6fc78af80c0bab99dc10c8dc4
kimhelp <operat8009@gmail.com>
2022-03-29 16:03:29 +0900

    메인카테고리 추가

--- front/routes/index.js
+++ front/routes/index.js
@@ -14,6 +14,9 @@ router.use('/reply',replyRouter);
 router.get('/',(req,res)=>{
     res.render('index.html');
 });
+router.get('/board_list',(req,res)=>{
+    res.render('board_list.html');
+});
 
 //이미지 불러오는 라우터
 router.use(express.static('views'));
