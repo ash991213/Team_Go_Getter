7c6a951cc746b6bec2bb14a8c64a6d38d488a095
ash991213 <ads1213@nate.com>
2022-03-30 16:15:24 +0900

    UPDATE DB

--- back/SQL/db.sql
+++ back/SQL/db.sql
@@ -21,26 +21,34 @@ CREATE TABLE user (
     INDEX idx_user_userid(userid)
 );
 
+CREATE TABLE point (
+    userid VARCHAR(32) NOT NULL,
+    b_point INT NOT NULL DEFAULT 0,
+    r_point INT NOT NULL DEFAULT 0,
+    FOREIGN KEY (userid) REFERENCES user (userid)
+);
+
 CREATE TABLE maincategory (
     m_idx INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(32) NOT NULL
+    m_name VARCHAR(32) NOT NULL
 );
 
 CREATE TABLE subcategory (
     s_idx INT PRIMARY KEY AUTO_INCREMENT,
     m_idx INT NOT NULL,
-    name VARCHAR(32) NOT NULL,
+    s_name VARCHAR(32) NOT NULL,
     FOREIGN KEY (m_idx) REFERENCES maincategory (m_idx)
 );
 
 CREATE TABLE board (
     b_idx INT PRIMARY KEY AUTO_INCREMENT,
-    s_idx INT NOT NULL,
+    s_idx INT,
     userid VARCHAR(32) NOT NULL,
     subject VARCHAR(64) NOT NULL,
     content TEXT,
     date TIMESTAMP NOT NULL,
     hit INT DEFAULT 0,
+    isActive BOOLEAN NOT NULL DEFAULT 1,
     FOREIGN KEY (userid) REFERENCES user (userid),
     FOREIGN KEY (s_idx) REFERENCES subcategory (s_idx)
 );
@@ -52,7 +60,7 @@ CREATE TABLE reply (
     content TEXT NOT NULL,
     depth INT NOT NULL DEFAULT 0,
     seq INT NOT NULL,
-    groupNum INT NOT NULL,
+    groupNum INT,
     date TIMESTAMP NOT NULL,
     FOREIGN KEY (userid) REFERENCES user (userid)
 );
@@ -67,8 +75,8 @@ CREATE TABLE intro (
 CREATE TABLE likes (
     l_idx INT PRIMARY KEY AUTO_INCREMENT,
     userid VARCHAR(32) NOT NULL,
-    b_idx INT NOT NULL,
-    r_idx INT NOT NULL,
+    b_idx INT,
+    r_idx INT,
     like_num INT NOT NULL DEFAULT 0,
     dislike_num INT NOT NULL DEFAULT 0,
     like_check INT NOT NULL DEFAULT 0,
@@ -86,7 +94,12 @@ CREATE TABLE file (
 
 CREATE TABLE hashtag (
     h_idx INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE board_hash (
+    h_idx INT NOT NULL,
     b_idx INT NOT NULL,
-    name VARCHAR(32) NOT NULL,
+    FOREIGN KEY (h_idx) REFERENCES hashtag (h_idx),
     FOREIGN KEY (b_idx) REFERENCES board (b_idx)
 );
--- back/SQL/tables/subcategory.sql
+++ back/SQL/tables/subcategory.sql
@@ -1,6 +1,6 @@
 CREATE TABLE subcategory (
     s_idx INT PRIMARY KEY AUTO_INCREMENT,
     m_idx INT NOT NULL,
-    name VARCHAR(32) NOT NULL,
+    s_name VARCHAR(32) NOT NULL,
     FOREIGN KEY (m_idx) REFERENCES maincategory (m_idx)
 );
