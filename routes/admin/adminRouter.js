2a4ad2edb671bd23da1ce8e66c67b7b3ae9830c3
kimhelp <99451708+kimhelp@users.noreply.github.com>
2022-03-30 14:28:08 +0900

    Delete routes directory

--- routes/admin/adminRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('./adminController.js')
-
-
-
-module.exports = router;
--- routes/board/boardController.js
+++
@@ -1,7 +0,0 @@
-exports.write = (req,res)=>{
-    res.render('board_write');
-};
-
-exports.edit = (req,res)=>{
-    res.render('board_edit');
-};
