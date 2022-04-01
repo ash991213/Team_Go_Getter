cfe0e604e61cfa6e46d4dd7eaa2aa5b7e790442e
ash991213 <ads1213@nate.com>
2022-04-01 15:33:31 +0900

    refactor:board (#87)

--- back/routes/board/boardController.js
+++ back/routes/board/boardController.js
@@ -2,6 +2,7 @@ const pool = require('../../models/db.js').pool;
 const { decodePayload } = require('../../utils/jwt.js');
 
 exports.GetWrite = async (req,res) => {
+    // 서브 카테고리
     const sql = 'SELECT * FROM subcategory'
 
     let response = {
@@ -39,24 +40,27 @@ exports.PostWrite = async (req,res) => {
 
     const hashtag = req.body
 
+    // 서브 카테고리 idx값 가져오기
     const [[result]] = `SELECT * FROM subcategory WHERE name = ${subcategory}`
     const s_idx = result.s_idx 
     
+    // 게시글 추가
     const sql = `INSERT INTO board(userid,subject,content,date,s_idx) VALUES (?,?,?,CURRENT_TIMESTAMP,?)`
-    const prepare = [userid,subject,content,s_idx] // s_idx
+    const prepare = [userid,subject,content,s_idx]
 
     let response = {
         errno:0
     }
 
     try{
-        const [result] = await pool.execute(sql,prepare)
-        const b_idx = result.insertId
+        const [result2] = await pool.execute(sql,prepare)
+        const b_idx = result2.insertId
 
         // 이미지 파일이 있으면 추가
         if ( files != [] )
         files.forEach( async v => {
             const sql2 = `INSERT INTO file(image,b_idx) VALUES ('${v}',${b_idx})`
+            await pool.execute(sql2)
         });
 
         // 해시태그 있으면 추가
@@ -65,7 +69,7 @@ exports.PostWrite = async (req,res) => {
             // 입력한 해시태그가 이미 있는지 체크
             const sql3 = `SELECT * FROM hashtag WHERE name='${v}'`
             const [result3] = await pool.execute(sql3)
-            // 없으면 해시태그 테이블에 추가
+            // 없으면 해시태그 테이블에 추가 후 게시판_해시 테이블에 추가
             if ( result3.length == 0 ){
                 const sql4 = `INSERT INTO hashtag(name) VALUES ('${v}')`
                 await pool.execute(sql4)
@@ -74,14 +78,14 @@ exports.PostWrite = async (req,res) => {
                 const sql5 = `INSERT INTO board_hash(h_idx,b_idx) VALUES (${h_idx},${b_idx})`
                 await pool.execute(sql5)
             } else {
-            // 게시판_해시 테이블에 추가
+            // 있으면 게시판_해시 테이블에만 추가
                 const h_idx = result3[0].h_idx
                 const sql5 = `INSERT INTO board_hash(h_idx,b_idx) VALUES (${h_idx},${b_idx})`
                 await pool.execute(sql5)
             }
         });
 
-        // 게시글 작성 point 추가
+        // 게시글 작성시 point 추가
         const [[result4]] = await pool.execute(`SELECT * FROM point WHERE userid = ${userid}`)
         const b_point = result4.b_point + 10
         await pool.execute(`UPDATE point SET b_point = ${b_point} WHERE userid = ${userid}`)
@@ -106,8 +110,10 @@ exports.list = async (req,res) => {
     // const token = req.cookies.user
     const userid = 'admin' // decodePayload(token).userid
 
+    // 전체 게시글
     const sql = 'SELECT * FROM board'
 
+    // 글보기 가능한 게시글
     const sql2 = 'SELECT * FROM board WHERE isActive = 1'
 
     let response = {
@@ -143,6 +149,7 @@ exports.mainList = async (req,res) => {
     // const token = req.cookies.user
     const userid = 'admin' // decodePayload(token).userid
 
+    // 메인 카테고리의 모든 게시글
     const sql = `SELECT a.b_idx, a.userid, a.subject, a.date, a.hit, a.s_idx, a.reply_count b.image, d.name, e.s_name, f.m_name
                  FROM board a
                  LEFT OUTER JOIN file AS b ON a.b_idx = b.b_idx
@@ -152,6 +159,7 @@ exports.mainList = async (req,res) => {
                  LEFT OUTER JOIN maincategory AS f ON e.m_idx = f.m_idx
                  WHERE f.m_idx = ${m_idx}`
 
+    // 메인 카테고리의 글보기 가능한 게시글
     const sql2 = `SELECT a.b_idx, a.userid, a.subject, a.date, a.hit, a.s_idx, a.reply_count, b.image, d.name, e.s_name, f.m_name
                  FROM board a
                  LEFT OUTER JOIN file AS b ON a.b_idx = b.b_idx
@@ -194,6 +202,7 @@ exports.subList = async (req,res) => {
     // const token = req.cookies.user
     const userid = 'admin' // decodePayload(token).userid
 
+    // 서브 카테고리의 모든 게시글
     const sql = `SELECT a.b_idx, a.userid, a.subject, a.date, a.hit, a.s_idx, a.reply_count, b.image, d.name, e.s_name
                  FROM board a
                  LEFT OUTER JOIN file AS b ON a.b_idx = b.b_idx
@@ -202,6 +211,7 @@ exports.subList = async (req,res) => {
                  LEFT OUTER JOIN subcategory AS e ON a.s_idx = e.s_idx
                  WHERE a.s_idx = ${s_idx}`
 
+    // 서브 카테고리의 글보기 가능한 게시글
     const sql2 = `SELECT a.b_idx, a.userid, a.subject, a.date, a.hit, a.s_idx, a.reply_count, b.image, d.name, e.s_name
                   FROM board a
                   LEFT OUTER JOIN file AS b ON a.b_idx = b.b_idx
@@ -365,12 +375,20 @@ exports.PostEdit = async (req,res) => {
     const { subject,content } = req.body
     const b_idx = 1 // req.query
 
-    const files = ['수정이미지1','수정이미지2','수정이미지3','수정이미지4','수정이미지5'] // req.files
+    const files = req.files
+    const filename = new Array()
+    if ( req.files.upload1 != undefined ) {filename.push(req.files.upload1[0].filename)}
+    if ( req.files.upload2 != undefined ) {filename.push(req.files.upload2[0].filename)}
+    if ( req.files.upload3 != undefined ) {filename.push(req.files.upload3[0].filename)}
+    if ( req.files.upload4 != undefined ) {filename.push(req.files.upload4[0].filename)}
+    if ( req.files.upload5 != undefined ) {filename.push(req.files.upload5[0].filename)}
 
-    const hashtag = ['테스트1','테스트2','테스트3','테스트4','테스트5'] // req.body // 해시태그 배열에 담아서 주세요.
+    const hashtag = ['테스트1','테스트2','테스트3','테스트4','테스트5'] // req.body
 
+    // 게시글 수정
     const sql = `UPDATE board SET content='${content}',subject='${subject}' WHERE b_idx=${b_idx}`
 
+    // 이미지 파일 삭제
     const sql2 = `DELETE FROM file WHERE b_idx=${b_idx}`
 
     let response = {
@@ -380,17 +398,16 @@ exports.PostEdit = async (req,res) => {
     try {
         const [result] = await pool.execute(sql)
 
-        // 이미지파일 전부 지우고 다시 추가
         await pool.execute(sql2)
 
         files.forEach( async v => {
+            // 이미지 파일 다시 추가
             const sql3 = `INSERT INTO file(image,b_idx) VALUES ('${v}',${b_idx})`
             await pool.execute(sql3)
         });
 
-        // 해시태그
+        // 해시태그가 없으면 모두 삭제
         if ( hashtag.length == 0 ){
-            console.log('삭제')
             const sql4=`DELETE a FROM board_hash AS a
                         LEFT OUTER JOIN hashtag AS b
                         ON a.h_idx = b.h_idx
@@ -399,6 +416,7 @@ exports.PostEdit = async (req,res) => {
             await pool.execute(sql4)
         }
         hashtag.forEach( async v => {
+            // 해시태그가 다른 게시글에 사용중인지 체크
             const sql = `SELECT a.b_idx, b.h_idx, c.name
                              FROM board a
                              LEFT OUTER JOIN board_hash AS b ON a.b_idx = b.b_idx
@@ -406,48 +424,46 @@ exports.PostEdit = async (req,res) => {
                              WHERE c.name = '${v}'
                              `
             const [result] = await pool.execute(sql)
-            console.log(result)
 
-            if ( result.length == 0 ) { // 사용중인 것인지 아닌지 확인하여 신규 / 수정 체크
-                console.log('신규')
+            if ( result.length == 0 ) {
+                // 사용중이지 않을경우 삭제
                 const sql = `DELETE FROM board_hash WHERE b_idx = ${b_idx}`
                 await pool.execute(sql)
                 // 입력한 해시태그가 이미 있는지 체크
-                const sql3 = `SELECT * FROM hashtag WHERE name='${v}'`
-                const [result3] = await pool.execute(sql3)
-                // 없으면 해시태그 테이블에 추가
+                const sql2 = `SELECT * FROM hashtag WHERE name='${v}'`
+                const [result2] = await pool.execute(sql2)
 
-                if ( result3.length == 0 ){
-                    const sql4 = `INSERT INTO hashtag(name) VALUES ('${v}')`
-                    await pool.execute(sql4)
-                    const [result3] = await pool.execute(sql3)
+                // 없으면 해시태그 테이블에 추가후 게시판_해시 테이블에 추가
+                if ( result2.length == 0 ){
+                    const sql3 = `INSERT INTO hashtag(name) VALUES ('${v}')`
+                    await pool.execute(sql3)
+                    const [result3] = await pool.execute(sql2)
                     const h_idx = result3[0].h_idx
-                    const sql5 = `INSERT INTO board_hash(h_idx,b_idx) VALUES (${h_idx},${b_idx})`
-                    await pool.execute(sql5)
+                    const sql4 = `INSERT INTO board_hash(h_idx,b_idx) VALUES (${h_idx},${b_idx})`
+                    await pool.execute(sql4)
                 } else {
-                // 게시판_해시 테이블에 추가
-                const h_idx = result3[0].h_idx
+                // 업으면 게시판_해시 테이블에만 추가
+                const h_idx = result2[0].h_idx
                 const sql5 = `INSERT INTO board_hash(h_idx,b_idx) VALUES (${h_idx},${b_idx})`
                 await pool.execute(sql5)
                 }
             } else {
-                console.log('수정')
                 const sql = `DELETE FROM board_hash WHERE b_idx = ${b_idx}`
                 await pool.execute(sql)
-                const sql3 = `SELECT * FROM hashtag WHERE name='${v}'`
-                const [result3] = await pool.execute(sql3)
-                // 없으면 해시태그 테이블에 추가
+                const sql2 = `SELECT * FROM hashtag WHERE name='${v}'`
+                const [result2] = await pool.execute(sql2)
 
-                if ( result3.length == 0 ){
-                    const sql4 = `INSERT INTO hashtag(name) VALUES ('${v}')`
-                    await pool.execute(sql4)
-                    const [result3] = await pool.execute(sql3)
+                // 없으면 해시태그 테이블에 추가
+                if ( result2.length == 0 ){
+                    const sql3 = `INSERT INTO hashtag(name) VALUES ('${v}')`
+                    await pool.execute(sql3)
+                    const [result3] = await pool.execute(sql2)
                     const h_idx = result3[0].h_idx
-                    const sql5 = `INSERT INTO board_hash(h_idx,b_idx) VALUES (${h_idx},${b_idx})`
-                    await pool.execute(sql5)
+                    const sql4 = `INSERT INTO board_hash(h_idx,b_idx) VALUES (${h_idx},${b_idx})`
+                    await pool.execute(sql4)
                 } else {
                 // 게시판_해시 테이블에 추가
-                const h_idx = result3[0].h_idx
+                const h_idx = result2[0].h_idx
                 const sql5 = `INSERT INTO board_hash(h_idx,b_idx) VALUES (${h_idx},${b_idx})`
                 await pool.execute(sql5)
                 }
@@ -477,6 +493,7 @@ exports.delete = async (req,res) => {
     // const token = req.cookies.user
     const userid = 'admin' // decodePayload(token).userid
 
+    // 게시글 , 게시글_해시 , 이미지 파일 삭제
     const sql = `DELETE a,b,c
                  FROM board AS a
                  LEFT OUTER JOIN board_hash AS b ON a.b_idx = b.b_idx
@@ -484,6 +501,7 @@ exports.delete = async (req,res) => {
                  WHERE a.b_idx = ${b_idx}
                  `
 
+    // 해시태그 삭제
     const sql2 = `DELETE a
                   FROM hashtag AS a
                   LEFT OUTER JOIN board_hash AS b
@@ -491,6 +509,7 @@ exports.delete = async (req,res) => {
                   WHERE b.h_idx IS NULL
                   `
 
+    // 본인인증
     const sql3 = `SELECT * FROM board WHERE b_idx = ${b_idx} AND userid = '${userid}'`
     const [result3] = await pool.execute(sql3)
 
@@ -519,11 +538,14 @@ exports.likes = async (req,res) => {
     // const token = req.cookies.user
     const userid = 'ash991213' // decodePayload(token).userid
 
+    // 해당 게시글에 좋아요를 눌렀나 체크
     const sql = `SELECT * FROM likes WHERE userid=? AND b_idx=?`
 
+    // 안눌렀으면 좋아요 등록
     const sql2 = `INSERT INTO likes (userid,b_idx,like_num,like_check)
                  VALUES (?,?,?,?)`
 
+    // 이미 눌렀으면 좋아요 취소
     const sql3 = `DELETE FROM likes WHERE userid=? AND b_idx=?`
 
     const prepare = [userid,b_idx]
@@ -573,11 +595,14 @@ exports.dislikes = async (req,res) => {
     // const token = req.cookies.user
     const userid = 'ash991213' // decodePayload(token).userid
 
+    // 해당 게시글의 싫어요를 눌렀나 체크
     const sql = `SELECT * FROM likes WHERE userid=? AND b_idx=?`
 
+    // 안눌렀을경우 싫어요 등록
     const sql2 = `INSERT INTO likes (userid,b_idx,dislike_num,like_check)
                  VALUES (?,?,?,?)`
 
+    // 이미 눌렀을경우 싫어요 취소
     const sql3 = `DELETE FROM likes WHERE userid=? AND b_idx=?`
 
     const prepare = [userid,b_idx]
@@ -624,6 +649,7 @@ exports.dislikes = async (req,res) => {
 exports.down = async (rep,res) => {
     const b_idx = 1 // req.body
 
+    // 해당 게시글 찾아오기
     const sql = `SELECT * FROM board WHERE b_idx = ${b_idx}`
 
     let response = {
@@ -632,10 +658,11 @@ exports.down = async (rep,res) => {
 
     try {
         const [result] = await pool.execute(sql)
-        console.log(result)
         if ( result[0].isActive == 1 ) {
+            // 게시글 보이기 상태일경우 숨기기
             await pool.execute(`UPDATE board SET isActive = 2 WHERE b_idx = ${b_idx}`)
         } else {
+            // 게시글 숨기기 상태일경우 보이기
             await pool.execute(`UPDATE board SET isActive = 1 WHERE b_idx = ${b_idx}`)
         }
 
@@ -648,6 +675,7 @@ exports.down = async (rep,res) => {
     res.json(response)
 }
 
+// 수정해야됌
 exports.find = async (req,res) => {
     const {subject, content, hashtag} = req.body
 
