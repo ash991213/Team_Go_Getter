b9691e8d18809f0ed5479c9584c128d8c462a78f
kimhelp <operat8009@gmail.com>
2022-04-05 11:02:06 +0900

    패키지 수정,프론트 서버 수정

--- front/server.js
+++ front/server.js
@@ -3,8 +3,7 @@ const nunjucks = require('nunjucks');
 const cookieParser = require('cookie-parser');
 const axios = require('axios');
 const webSocket = require('ws');
-const session = require('express-session')
-const Memorystore = require('memorystore')(session)
+
 const app = express();
 
 const router = require('./routes/index.js');
@@ -18,18 +17,6 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
-const maxAge = 6*600000
-let sessionObj = {
-    secret: "kim1",
-    resave : false,
-    saveUninitialized: true,
-    store: new Memorystore({ checkPeriod: maxAge}),
-    cookie:{
-        maxAge:maxAge
-    }
-}
-
-app.use(session(sessionObj))
 app.use(router);
 
 app.listen(3000);
