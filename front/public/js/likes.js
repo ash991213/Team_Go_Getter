73fced7fe1dcfec585b94037b374ad4b4501ee3d
kimhelp <operat8009@gmail.com>
2022-04-06 22:52:32 +0900

    디자인 수정

--- front/public/js/likes.js
+++
@@ -1,5 +0,0 @@
-('.like-container > .feeling_a, .dislike-container  > .feeling_a').on('click', function() {
-    event.preventDefault();
-    ('.active').removeClass('active');
-    (this).addClass('active');
-});
